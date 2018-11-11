2f5472f88cf529a1096dc9f1997c886e92aa3188
VicenzoMartinelli <32465152+VicenzoMartinelli@users.noreply.github.com>
2018-11-11 13:27:04 -0200

    Add project files.

---
+++ SuperHeroes_Softfocus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuperHeroes_Softfocus.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
